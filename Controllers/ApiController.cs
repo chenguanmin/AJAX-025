73a44b2d7e569b953dcd0c698edb8f419e86197f
冠名 陳 <c121474790@gmail.com>
2022-07-04 10:31:14 +0800

    Content

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJAX_025.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("<h2>hello ajax</h2>","text/html", System.Text.Encoding.UTF8);
+        }
+    }
+}
